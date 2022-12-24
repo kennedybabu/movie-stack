c695e2c0563c547a14e061c80668ddf0076c602d
kennedybabu <kennedynjogu8@gmail.com>
2022-12-24 22:25:05 +0300

    feature: style the movie component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import Movie from "./components/Movie";
 
 function App() {
   const [popular, setPopular] = useState([])
@@ -11,14 +12,17 @@ function App() {
   const fetchPopular = async () => {
     const data = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=51b2b5d5f941e522b58e755e059cf6b2&language=en-US&page=1`)
     const movies = await data.json()
+    console.log(movies)
     setPopular(movies.results)
   }
 
 
   return (
-    <div className="App">
-      <div className="popular-movies">
-
+    <div className="w-full py-[5%]">
+      <div className="gap-[2rem] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:px-[250px]">
+          {popular.map((movie) => {
+            return <Movie movie={movie} key={movie.id}/>
+          })}
       </div>
     </div>
   );
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Movie = () => {
+const Movie = ({movie}) => {
   return (
-    <div>
-        
+    <div className='w-[200px]'>
+      <h3 className='text-[.8rem] ml-1 font-bold mb-1'>{movie.name}</h3>
+      <img className="img h-[40vh] rounded-[1rem] w-full mb-[1rem]" src={"https://image.tmdb.org/t/p/w300"+movie.backdrop_path} alt="/" />
     </div>
   )
 }
