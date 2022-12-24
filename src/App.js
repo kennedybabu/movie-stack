b2cbb443d87b04a4e05aa9422617418ab0958f10
kennedybabu <kennedynjogu8@gmail.com>
2022-12-24 20:02:58 +0300

    feature: create the movie component

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ function App() {
   const fetchPopular = async () => {
     const data = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=51b2b5d5f941e522b58e755e059cf6b2&language=en-US&page=1`)
     const movies = await data.json()
-    console.log(movies)
     setPopular(movies.results)
   }
 
 
   return (
     <div className="App">
-      <p>here and there...</p>
+      <div className="popular-movies">
+
+      </div>
     </div>
   );
 }
